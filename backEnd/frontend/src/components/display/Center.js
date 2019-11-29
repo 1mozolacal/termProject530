86b8d19d8225f5e217939ae5b50ae51466c9c12b
Calvin Mozola <calvin@stuntfactory.com>
2019-11-29 18:47:26 -0500

    back up to date

--- backEnd/frontend/src/components/display/Center.js
+++ backEnd/frontend/src/components/display/Center.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getInfo } from "../../actions/centerAction";
@@ -14,9 +14,31 @@ export class center extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Center form</h1>
-      </div>
+      <Fragment>
+        <h1>title</h1>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>ticker</th>
+              <th>op</th>
+              <th>close</th>
+              <th>volume</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.object.map(item => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.ticker}</td>
+                <td>{item.op}</td>
+                <td>{item.close}</td>
+                <td>{item.volume}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
     );
   }
 }
