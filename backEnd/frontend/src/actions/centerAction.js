fb51ea7786664064712783ee2f0e9e42f582509c
Calvin Mozola <calvin@stuntfactory.com>
2019-12-01 13:58:36 -0500

    Form posting but getting 403

--- backEnd/frontend/src/actions/centerAction.js
+++ backEnd/frontend/src/actions/centerAction.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_INFO, DELETE_PICTURE } from "./types";
+import { GET_INFO, DELETE_PICTURE, MAKE_PICTURE } from "./types";
 
 //gets all the data
 export const getInfo = () => dispatch => {
@@ -23,4 +23,16 @@ export const deletePicture = id => dispatch => {
       payload: id
     });
   });
+};
+
+export const makePicture = object => dispatch => {
+  axios
+    .post("/api/pictures/", object)
+    .then(res => {
+      dispatch({
+        type: MAKE_PICTURE,
+        payload: res.data
+      });
+    })
+    .catch(err => console.log(err));
 };
