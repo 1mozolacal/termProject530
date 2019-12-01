7492c68889c539431023a5f0dcf3d4a5a156ba1e
Calvin Mozola <calvin@stuntfactory.com>
2019-12-01 12:47:14 -0500

    Added the form to add a new picture

--- backEnd/frontend/src/actions/centerAction.js
+++ backEnd/frontend/src/actions/centerAction.js
@@ -5,7 +5,7 @@ import { GET_INFO, DELETE_PICTURE } from "./types";
 //gets all the data
 export const getInfo = () => dispatch => {
   axios
-    .get("/pics/")
+    .get("/api/pictures/")
     .then(res => {
       dispatch({
         type: GET_INFO,
@@ -17,7 +17,7 @@ export const getInfo = () => dispatch => {
 
 //delete entry
 export const deletePicture = id => dispatch => {
-  axios.delete(`/pics/${id}/`).then(res => {
+  axios.delete(`/api/pictures/${id}/`).then(res => {
     dispatch({
       type: DELETE_PICTURE,
       payload: id
